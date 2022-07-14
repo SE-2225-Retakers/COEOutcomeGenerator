3594be7fa4ecc63a0f4cbe7a1e7091882a3f9f8c
George Jose Abaygar <georgejoseabaygar@gmail.com>
2022-07-15 07:07:16 +0800

    Initialized Firebase

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,8 +2,29 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Form, Container } from 'react-bootstrap';
 
+function LoginForm() {
+    return (
+        <div className="card-body">
+        {/* <div className="form-floating mb-3">
+            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name='acc_email' />
+            <label for="floatingInput">Email address</label>
+        </div>
+        <div className="form-floating">
+            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" name='acc_password' />
+            <label for="floatingPassword">Password</label>
+        </div > */}
+        <div className="text-center">
+            <button className="btn btn-primary text-right mt-4" >
+                Log In With Google
+            </button>
+        </div>
+        <p className="text-center mt-4">
+            <Link to="/signup" class="active">Create</Link>
+        </p>
+    </div>
+    )
 
-
+}
 
 export default function LoginPage() {
     return (
@@ -14,24 +35,7 @@ export default function LoginPage() {
                         <h1 className="text-center mt-5">Canvas</h1>
                         <h2 className="text-center mt-3">Log In</h2>
                         <img src="" className="card-img-top img-fluid" />
-                        <div className="card-body">
-                            <div className="form-floating mb-3">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" name='acc_email' />
-                                <label for="floatingInput">Email address</label>
-                            </div>
-                            <div className="form-floating">
-                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" name='acc_password' />
-                                <label for="floatingPassword">Password</label>
-                            </div >
-                            <div className="text-center">
-                                <button className="btn btn-primary text-right mt-4" >
-                                    Log In
-                                </button>
-                            </div>
-                            <p className="text-center mt-4">
-                                <Link to="/signup" class="active">Create</Link>
-                            </p>
-                        </div>
+                        <LoginForm/>
                     </div>
                 </div>
             </Form>
