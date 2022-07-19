af3a3607011bf4b34569cfdc979e457e8b47e643
George Jose Abaygar <georgejoseabaygar@gmail.com>
2022-07-20 00:19:57 +0800

    Refactors signInWithGoogle

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Form, Container } from 'react-bootstrap';
-import { auth } from '../firebase-config.js';
-import {  GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-
-function LoginForm() {
-    const signInWithGoogle = () => {
-        const provider = new GoogleAuthProvider();
-        signInWithPopup(auth, provider)
-        .then((result) => {
-          console.log(result);
-        }).catch((error) => {
-          console.log(error);
-        });
-      }
-    return (
-        <Container>
-        <button onClick={signInWithGoogle}>
-            log In With Google
-        </button>
-        </Container>
-    )
-
-}
+import { signInWithGoogle } from '../API/authWithEmail.js';
 
 export default function LoginPage() {
     return (
@@ -32,8 +11,11 @@ export default function LoginPage() {
                     <div className="col-11 col-md-7 col-lg-4 mx-0 mb-6">
                         <h1 className="text-center mt-5">Canvas</h1>
                         <h2 className="text-center mt-3">Log In</h2>
-                        <img src="" className="card-img-top img-fluid" />
-                        <LoginForm/>
+                    </div>
+                    <div className="text-center">
+                        <button className="btn btn-primary text-right mt-4" onClick={await signInWithGoogle}>
+                            Log In With Google
+                        </button>
                     </div>
                 </div>
             </Form>
