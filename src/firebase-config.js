2ae6a877141a36009c11cbd9f457bed5f0febd1c
George Jose Abaygar <georgejoseabaygar@gmail.com>
2022-07-21 00:17:41 +0800

    Fixes popup for SSO

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 //7~oHVH6RC7kfiCDAhaqgG8dkz9G5qn0DxuLllxG8EAU8zQVnYZWygQUv8Aq8UjJjCb
 
 const firebaseConfig = {
@@ -13,9 +13,20 @@ const firebaseConfig = {
     appId: "1:393155624782:web:454c69dc6eca67774d681f",
     measurementId: "G-P9KPHEKN13"
   };
-  
+
+export const googleProvider = new GoogleAuthProvider();
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-const db = getFirestore();
+const db = getFirestore(app);
 export const auth = getAuth(app);
+
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(auth, googleProvider);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Form, Container } from 'react-bootstrap';
-import { signInWithGoogle } from '../API/authWithEmail.js';
+import { signInWithGoogle } from '../firebase-config';
 
-export default function LoginPage() {
-    return (
-        <Container>
-            <Form>
-                <div className="row justify-content-center">
-                    <div className="col-11 col-md-7 col-lg-4 mx-0 mb-6">
-                        <h1 className="text-center mt-5">Canvas</h1>
-                        <h2 className="text-center mt-3">Log In</h2>
-                    </div>
-                    <div className="text-center">
-                        <button className="btn btn-primary text-right mt-4" onClick={await signInWithGoogle}>
-                            Log In With Google
-                        </button>
-                    </div>
-                </div>
-            </Form>
-        </Container>
-    );
+export default class LoginPage extends React.Component {
+    render() {
+        return (
+        <div className="row justify-content-center">
+            <div className="col-11 col-md-7 col-lg-4 mx-0 mb-6">
+                <h1 className="text-center mt-5">Canvas</h1>
+                <h2 className="text-center mt-3">Log In</h2>
+            </div>
+            <div className="text-center">
+                <button className="btn btn-primary text-right mt-4" onClick={signInWithGoogle}>
+                    Log In With Google
+                </button>
+            </div>
+        </div>
+        )
+    };
 };
 
